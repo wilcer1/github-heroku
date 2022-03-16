da5014ba06d365504290f98206ae3f598e5f05b8
wilcer1 <wille.cervin@live.se>
2022-03-16 13:17:44 +0100

    .

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(client));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5342;
+
+app.listen(PORT, () =>{
+    console.log("server running on port " + PORT);
+});
